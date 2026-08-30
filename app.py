2f07d5589c6dcf64bec1fab86a26cdfaaf1c0ad7
Chen <153264405+UniChen03@users.noreply.github.com>
2026-08-29 23:27:57 -0700

    Show CPU and NVMe temperatures

--- app.py
+++ app.py
@@ -16,14 +16,23 @@ def status():
 
     try:
         temperatures = psutil.sensors_temperatures()
-        temperature = None
+
+        cpu_temperature = None
+        nvme_temperature = None
 
         for sensor in temperatures.get("coretemp", []):
             if sensor.label == "Package id 0":
-                temperature = round(sensor.current, 1)
+                cpu_temperature = round(sensor.current, 1)
                 break
+
+        for sensor in temperatures.get("nvme", []):
+            if sensor.label == "Composite":
+                nvme_temperature = round(sensor.current, 1)
+                break
+
     except Exception:
-        temperature = None
+        cpu_temperature = None
+        nvme_temperature = None
 
     try:
         docker_client = docker.from_env()
@@ -32,18 +41,20 @@ def status():
         docker_running = None
 
     return jsonify(
-        uptime_seconds=int(time.time() - psutil.boot_time()),
-        cpu_percent=psutil.cpu_percent(interval=0.2),
+    uptime_seconds=int(time.time() - psutil.boot_time()),
+    cpu_percent=psutil.cpu_percent(interval=0.2),
 
-        memory_used=memory.used,
-        memory_total=memory.total,
+    memory_used=memory.used,
+    memory_total=memory.total,
 
-        system_disk_used=system_disk.used,
-        system_disk_total=system_disk.total,
+    system_disk_used=system_disk.used,
+    system_disk_total=system_disk.total,
 
-        data_disk_used=data_disk.used,
-        data_disk_total=data_disk.total,
+    data_disk_used=data_disk.used,
+    data_disk_total=data_disk.total,
 
-        temperature=temperature,
-        docker_running=docker_running,
-    )
+    cpu_temperature=cpu_temperature,
+    nvme_temperature=nvme_temperature,
+
+    docker_running=docker_running,
+)
